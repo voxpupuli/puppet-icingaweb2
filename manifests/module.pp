a14122c33cd10d094b8433473603e59e9b3a968e
Markus Frosch <markus@lazyfrosch.de>
2015-03-16 17:07:06 +0100

    Implement defined type for modules

---
+++ manifests/module.pp
@@ -0,0 +1,35 @@
+# == Define: icingaweb2::module
+#
+define icingaweb2::module(
+  $ensure = 'enabled',
+) {
+
+  validate_re($ensure, '^(en|dis)abled$', 'ensure must be one of: enabled or disabled')
+
+  include ::icingaweb2
+
+  File {
+    owner => $::icingaweb2::config_user,
+    group => $::icingaweb2::config_group,
+  }
+
+  if $ensure == 'disabled' {
+    $symlink = absent
+  }
+  else {
+    $symlink = link
+  }
+
+  file { 'icingaweb2-module-symlink':
+    ensure => $symlink,
+    path   => "${icingaweb2::config_dir}/enabledModules/${name}",
+    target => "${icingaweb2::web_root}/modules/${name}",
+  }
+
+  file { 'icingaweb2-module-config':
+    ensure => directory,
+    path   => "${icingaweb2::config_dir}/modules/${name}",
+    mode   => $::icingaweb2::config_dir_mode,
+  }
+
+}
