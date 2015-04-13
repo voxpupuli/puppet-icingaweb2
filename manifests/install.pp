ef29032af7716549ff277028b4ae1a012873750b
Bodo Schulz <support@xanhaem.de>
2015-04-13 20:55:45 +0200

    cleanup

--- manifests/install.pp
+++ manifests/install.pp
@@ -39,17 +39,5 @@ class icingaweb2::install {
     }
   }
 
-#       #Load the MySQL DB schema:
-#       exec { 'mysql_schema_load':
-#         user    => 'root',
-#         path    => '/usr/bin:/usr/sbin:/bin/:/sbin',
-#         command => "mysql -u ${db_user} -p${db_password} ${db_name} < ${server_db_schema_path} && touch /etc/icinga2/mysql_schema_loaded.txt",
-#         creates => '/etc/icinga2/mysql_schema_loaded.txt',
-#         require => Class['icinga2::server::install::packages'],
-#       }
-
-  # mysql -uicinga2 -ppassword icinga2_auth < /usr/share/doc/icingaweb2/schema/mysql.schema.sql
-
-
 }
 
