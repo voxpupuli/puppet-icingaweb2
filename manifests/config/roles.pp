12ce6534ee0dbff934abffe0da1df9fe952427c1
jweakley <j.weakley@nib.com.au>
2015-05-03 12:02:06 +1000

    Don't put blank host/service filters in roles.ini
    refs #9211

--- manifests/config/roles.pp
+++ manifests/config/roles.pp
@@ -36,12 +36,20 @@ define icingaweb2::config::roles (
     section => $role_name,
     setting => 'monitoring/hosts/filter',
     value   => "\"${role_host_filter}\"",
+	ensure  => $role_host_filter ? {
+	  undef => absent,
+      default => present,
+	}
   }
 
   ini_setting { "icingaweb2 roles ${title} service filter":
     section => $role_name,
     setting => 'monitoring/services/filter',
     value   => "\"${role_service_filter}\"",
+	ensure  => $role_service_filter ? {
+	  undef => absent,
+      default => present,
+	}
   }
 }
 
