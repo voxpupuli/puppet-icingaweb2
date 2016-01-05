af33ea765f7e34c01b187251407883bda8a9e436
Jan Collijs <jan.collijs@inuits.eu>
2016-01-05 14:39:13 +0100

    Feature: added monitoring module

---
+++ manifests/mod/monitoring.pp
@@ -0,0 +1,66 @@
+# == Class icingaweb2::mod::monitoring
+#
+class icingaweb2::mod::monitoring (
+  $web_root = $::icingaweb2::params::web_root,
+) {
+  require ::icingaweb2
+
+  File {
+    require => Class['::icingaweb2::config'],
+    owner => $::icingaweb2::config_user,
+    group => $::icingaweb2::config_group,
+    mode  => $::icingaweb2::config_file_mode,
+  }
+
+  file { "${::icingaweb2::config_dir}/modules/monitoring":
+    ensure => directory,
+    mode   => $::icingaweb2::config_dir_mode;
+  }
+
+  Ini_Setting {
+    ensure  => present,
+    require => File["${::icingaweb2::config_dir}/modules/monitoring"],
+  }
+
+  ini_setting { 'security settings':
+    section => 'security',
+    setting => 'protected_customvars',
+    value   => '"*pw*,*pass*,community"',
+    path    => "${::icingaweb2::config_dir}/modules/monitoring/config.ini",
+  }
+
+  ini_setting { 'backend ido setting':
+    section => 'icinga_ido',
+    setting => 'type',
+    value   => 'ido',
+    path    => "${::icingaweb2::config_dir}/modules/monitoring/backends.ini",
+  }
+
+  ini_setting { 'backend resource setting':
+    section => 'icinga_ido',
+    setting => 'resource',
+    value   => 'icinga_ido',
+    path    => "${::icingaweb2::config_dir}/modules/monitoring/backends.ini",
+  }
+
+  ini_setting { 'command transport setting':
+    section => 'icinga2',
+    setting => 'transport',
+    value   => 'local',
+    path    => "${::icingaweb2::config_dir}/modules/monitoring/commandtransports.ini",
+  }
+
+  ini_setting { 'command transport path setting':
+    section => 'icinga2',
+    setting => 'path',
+    value   => '/var/run/icinga2/cmd/icinga2.cmd',
+    path    => "${::icingaweb2::config_dir}/modules/monitoring/commandtransports.ini",
+  }
+
+  file { "${::icingaweb2::config_dir}/enabledModules/monitoring":
+    ensure => link,
+    target => '/usr/share/icingaweb2/modules/monitoring'
+  }
+
+}
+
