fed3f676d9555c74ccf070dcdafe05c84f9d2e1f
Bugala Piotr <Piotr.Bugala@igt.com>
2016-11-09 12:58:03 +0100

    Make sure that mysql commands execute after file is created

--- manifests/initialize.pp
+++ manifests/initialize.pp
@@ -20,12 +20,14 @@ class icingaweb2::initialize {
             exec { 'create db scheme':
               command => "mysql --defaults-file='/root/.my.cnf' ${::icingaweb2::web_db_name} < ${sql_schema_location}",
               unless  => "mysql --defaults-file='/root/.my.cnf' ${::icingaweb2::web_db_name} -e \"SELECT 1 FROM icingaweb_user LIMIT 1;\"",
-              notify  => Exec['create web user']
+              notify  => Exec['create web user'],
+              require => File[$::icingaweb2::db_pasword_file]
             }
 
             exec { 'create web user':
               command     => "mysql --defaults-file='/root/.my.cnf' ${::icingaweb2::web_db_name} -e \" INSERT INTO icingaweb_user (name, active, password_hash) VALUES ('icingaadmin', 1, '\\\$1\\\$EzxLOFDr\\\$giVx3bGhVm4lDUAw6srGX1');\"",
               refreshonly => true,
+              require     => File[$::icingaweb2::db_pasword_file]
             }
           }
 
