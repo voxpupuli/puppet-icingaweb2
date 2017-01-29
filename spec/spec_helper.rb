68ff1ea0f01ec2eb969ee0b7d2a5c3dfedf01383
Markus Frosch <markus.frosch@icinga.com>
2017-01-29 10:47:31 +0100

    Move coverage to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,8 @@ RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
   c.mock_with :mocha
+
+  at_exit { RSpec::Puppet::Coverage.report! }
 end
 
 def centos_facts
