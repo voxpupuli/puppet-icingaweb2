e3e41f5abf0febcd597f875f3359254cf412d2a6
intrigeri <intrigeri@boum.org>
2017-04-16 09:52:33 +0000

    Adjust package dependencies for Debian Stretch and newer.

--- manifests/params.pp
+++ manifests/params.pp
@@ -86,14 +86,25 @@ class icingaweb2::params {
       $pkg_list                          = ['icingaweb2']
       $web_root                          = '/usr/share/icingaweb2'
 
-      $pkg_deps = [
-        'php5-gd',
-        'php5-imagick',
-        'php5-intl',
-        'php5-ldap',
-        'php5-mysql',
-        'php5-pgsql',
-      ]
+      if $::operatingsystemmajrelease >= 9 {
+        $pkg_deps = [
+          'php-gd',
+          'php-imagick',
+          'php-intl',
+          'php-ldap',
+          'php-mysql',
+          'php-pgsql',
+        ]
+      } else {
+        $pkg_deps = [
+          'php5-gd',
+          'php5-imagick',
+          'php5-intl',
+          'php5-ldap',
+          'php5-mysql',
+          'php5-pgsql',
+        ]
+      }
     }
 
     default: {
