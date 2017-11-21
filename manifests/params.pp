09c405e962f6aa62162344d19a36962a3087fceb
Richard Paul <richard@primarysite.net>
2017-11-21 16:31:40 +0000

    Initial variable configuration for FreeBSD

--- manifests/params.pp
+++ manifests/params.pp
@@ -14,30 +14,43 @@
 class icingaweb2::params {
 
   $package     = 'icingaweb2'
-  $conf_dir    = '/etc/icingaweb2'
-  $module_path = '/usr/share/icingaweb2/modules'
 
   case $::facts['os']['family'] {
     'redhat': {
+      $conf_dir              = '/etc/icingaweb2'
       $conf_user             = 'apache'
       $conf_group            = 'icingaweb2'
+      $module_path           = '/usr/share/icingaweb2/modules'
       $schema_dir            = '/usr/share/doc/icingaweb2/schema'
       $gettext_package_name  = 'gettext'
     } # RedHat
 
     'debian': {
+      $conf_dir             = '/etc/icingaweb2'
       $conf_user            = 'www-data'
       $conf_group           = 'icingaweb2'
+      $module_path          = '/usr/share/icingaweb2/modules'
       $schema_dir           = '/usr/share/icingaweb2/etc/schema'
       $gettext_package_name = 'gettext'
     } # Debian
 
     'suse': {
+      $conf_dir             = '/etc/icingaweb2'
       $conf_user            = 'wwwrun'
       $conf_group           = 'icingaweb2'
+      $module_path          = '/usr/share/icingaweb2/modules'
       $schema_dir           = '/usr/share/doc/icingaweb2/schema'
       $gettext_package_name = 'gettext-tools'
     } # Suse
+
+    'FreeBSD': {
+      $conf_dir             = '/usr/local/etc/icingaweb2'
+      $conf_user            = 'www'
+      $conf_group           = 'icingaweb2'
+      $module_path          = '/usr/local/www/icingaweb2/modules'
+      $schema_dir           = '/usr/local/www/icingaweb2/etc/schema'
+      $gettext_package_name = 'gettext-runtime'
+    } # FreeBSD
 
     default: {
       fail("Your plattform ${::facts['os']['family']} is not supported, yet.")
