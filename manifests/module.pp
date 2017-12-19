23ac7485fa4f9459f1272d18e0f2218c7b5a8582
marco.fretz <marco.fretz@vshn.ch>
2017-12-19 22:41:10 +0100

    Make sure module conf dir is created before inisettings are managed

--- manifests/module.pp
+++ manifests/module.pp
@@ -76,7 +76,11 @@ define icingaweb2::module(
     $ensure_module_config_dir = 'directory'
     $ensure_vcsrepo = 'present'
 
-    create_resources('icingaweb2::inisection', $settings)
+    create_resources('icingaweb2::inisection', $settings,
+      {
+        require => File["${conf_dir}/modules/${module}"]
+      }
+    )
   } else {
     $ensure_module_enabled = 'absent'
     $ensure_module_config_dir = 'absent'
