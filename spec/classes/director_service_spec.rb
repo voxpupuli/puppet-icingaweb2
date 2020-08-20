4fafce24fe6677f190f235b8783a723851dc96b3
Joshua Hoblitt <josh@hoblitt.com>
2020-08-20 13:49:56 -0700

    check if directory service is passed the correct template

--- spec/classes/director_service_spec.rb
+++ spec/classes/director_service_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
-describe('icingaweb2::module::director::service', :type => :class) do
-  let(:pre_condition) { [
+describe('icingaweb2::module::director::service', type: :class) do
+  let(:pre_condition) do
+    [
       "class { 'icingaweb2': }",
       "class { 'icingaweb2::module::director': }",
-  ] }
+    ]
+  end
 
   on_supported_os.each do |os, facts|
     context "on #{os}" do
@@ -13,17 +15,27 @@ describe('icingaweb2::module::director::service', :type => :class) do
       end
 
       context "#{os} with defaults" do
-        it { is_expected.to contain_user('icingadirector')
-          .with({ 'ensure' => 'present', 'gid' => 'icingaweb2', 'shell' => '/bin/false' }).that_comes_before('Systemd::Unit_file[icinga-director.service]') }
-        it { is_expected.to contain_systemd__unit_file('icinga-director.service').that_notifies('Service[icinga-director]') }
-        it { is_expected.to contain_service('icinga-director')
-          .with({
-            'ensure' => 'running',
-            'enable' => true }) }
-
+        it do
+          is_expected.to contain_user('icingadirector')
+            .with(
+              'ensure' => 'present',
+              'gid' => 'icingaweb2',
+              'shell' => '/bin/false',
+            ).that_comes_before('Systemd::Unit_file[icinga-director.service]')
+        end
+        it do
+          is_expected.to contain_systemd__unit_file('icinga-director.service').with(
+            content: %r{[Unit]},
+          ).that_notifies('Service[icinga-director]')
+        end
+        it do
+          is_expected.to contain_service('icinga-director')
+            .with(
+              'ensure' => 'running',
+              'enable' => true,
+            )
+        end
       end
     end
-
   end
 end
-
