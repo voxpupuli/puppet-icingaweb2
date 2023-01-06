0be714afee9bbcf00f2f8e3dbadf7a82977f6373
Lennart Betz <lennart.betz@netways.de>
2023-01-06 18:50:04 +0100

    fix #331 Add support for Role permission refusals, parent and unrestricted

--- spec/defines/role_spec.rb
+++ spec/defines/role_spec.rb
@@ -14,7 +14,7 @@ describe('icingaweb2::config::role', type: :define) do
         facts
       end
 
-      context "#{os} with users => 'bob, pete' and permissions => '*'" do
+      context "#{os} with users and permissions" do
         let(:params) { { users: 'bob, pete', permissions: '*' } }
 
         it {
@@ -24,13 +24,25 @@ describe('icingaweb2::config::role', type: :define) do
         }
       end
 
-      context "#{os} with users => 'bob, pete', permissions => 'module/monitoring', filters => {'monitoring/filter/objects' => 'host_name=linux-*'}" do
-        let(:params) { { users: 'bob, pete', permissions: 'module/monitoring', filters: { 'monitoring/filter/objects' => 'host_name=linux-*' } } }
+      context "#{os} with users, permissions, refusals and filters" do
+        let(:params) do
+          {
+            users: 'bob, pete',
+            permissions: 'module/monitoring',
+            refusals: 'monitoring/commands/schedule-check',
+            filters: { 'monitoring/filter/objects' => 'host_name=linux-*' }
+          }
+        end
 
         it {
           is_expected.to contain_icingaweb2__inisection('role-myrole')
             .with_target('/etc/icingaweb2/roles.ini')
-            .with_settings('users' => 'bob, pete', 'permissions' => 'module/monitoring', 'monitoring/filter/objects' => 'host_name=linux-*')
+            .with_settings(
+              'users' => 'bob, pete',
+              'permissions' => 'module/monitoring',
+              'refusals' => 'monitoring/commands/schedule-check',
+              'monitoring/filter/objects' => 'host_name=linux-*',
+            )
         }
       end
     end
